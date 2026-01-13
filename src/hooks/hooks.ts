10eb3c0bf8ffd083ec4581f978f52e6b4675b85a
Bhushan <bhushan.dhamange@gmail.com>
2026-01-14 00:10:11 +0530

    added multiple cucumber report

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,14 +1,46 @@
-import { After, Before } from "@cucumber/cucumber";
-import { chromium, Browser, Page } from "@playwright/test";
+import { After, AfterAll, AfterStep, Before, BeforeAll, Status } from "@cucumber/cucumber";
+import { chromium, Browser, BrowserContext, Page } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
 
-// Before(async function () {
-//     const browser: Browser = await chromium.launch({ headless: false });
-//     const page: Page = await browser.newPage();
-//     pageFixture.page = page;
-// });
+let browser: Browser;
+let page: Page;
+let context: BrowserContext;
 
-// After(async function () {
-//     pageFixture.page.close();
-//     await pageFixture.page.context().browser()?.close();
-// });
+BeforeAll(async function () {
+    console.log("----- Launching Browser -----");
+    browser = await chromium.launch({ headless: false });
+});
+
+Before(async function () {
+    console.log("----- Creating New Context and Page -----");
+    context = await browser.newContext();
+    page = await context.newPage();
+    pageFixture.page = page;
+});
+
+After(async function ({pickle, result}) {
+    //Screenshot only if scenario fails
+    // if(result?.status == Status.FAILED){
+    //     const screenshot = await pageFixture.page.screenshot({ path: `test-result/screenshots/${pickle.name.replace(/ /g, "_")}.png`, fullPage: true });
+    //     this.attach(screenshot, "image/png");
+    // }
+
+    //Screenshot after every scenario
+    // const screenshot = await pageFixture.page.screenshot({ path: `test-result/screenshots/${pickle.name.replace(/ /g, "_")}.png`, fullPage: true });
+    // this.attach(screenshot, "image/png");
+
+    console.log("----- Closing Page and Context -----");
+    pageFixture.page.close();
+    await context.close();
+});
+
+AfterStep(async function ({pickle}) {
+    console.log("----- Taking Screenshot After Step -----");
+    const screenshot = await pageFixture.page.screenshot({ path: `test-result/screenshots/${pickle.name.replace(/ /g, "_")}.png`, fullPage: true });
+    this.attach(screenshot, "image/png");
+});
+
+AfterAll(async function () {
+    console.log("----- Closing Browser -----");
+    await browser.close();
+});
--- src/test/features/hooks.feature
+++ src/test/features/hooks.feature
@@ -1,12 +1,12 @@
-Feature: Hooks Testing
+# Feature: Hooks Testing
 
-Scenario: Hooks scenario 1
-   Given I am on page A
-   When I am on Page B
-   Then I am on Page C
-   And I am on Page D
+# Scenario: Hooks scenario 1
+#    Given I am on page A
+#    When I am on Page B
+#    Then I am on Page C
+#    And I am on Page D
 
-Scenario: Hooks scenario 2
-   Given I am on page E
-   When I am on Page F
-   Then I am on Page G
+# Scenario: Hooks scenario 2
+#    Given I am on page E
+#    When I am on Page F
+#    Then I am on Page G
